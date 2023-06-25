d7a61291914452a74fddaa8783c21b012a9ad82c
Robert Murhamer <wi22b073@technikum-wien.at>
2023-06-25 15:49:43 +0200

    added controller and service

---
+++ src/main/java/com/example/SLMExam/controller/DateController.java
@@ -0,0 +1,27 @@
+package com.example.SLMExam.controller;
+
+import com.example.SLMExam.service.DateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+@RestController
+@RequestMapping("/api")
+public class DateController {
+    private final DateService dateService;
+    @Autowired
+    public DateController(DateService service){
+        this.dateService = service;
+    }
+    @GetMapping("/time")
+    public LocalTime getTime(){
+        return dateService.getTime();
+    }
+    @GetMapping("/date")
+    public String getDate(@RequestParam(required = false) String format){
+        return dateService.getDate(format);
+    }
+}
